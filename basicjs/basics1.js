99667a239e351494ba39ab1450f22f0aa3e76fbe
vercisingh <40191927+vercisingh@users.noreply.github.com>
2024-02-01 06:14:08 +0000

    practiced variables/keywords primitive , datatypes, typeof datatypes

---
+++ basicjs/basics1.js
@@ -0,0 +1,26 @@
+const accountId = 144553;
+let accountNumber= 345;
+console.log(accountId,accountNumber);
+console.table([accountId,accountNumber]);
+
+// use strict  (treats all js code as newer version) 
+
+//alert (3+3);// ReferenceError: alert is not defined (Bcz we are using node.js , not browser)
+
+//datatype : 
+
+// Primitive datatypes
+
+//number : 2 to power 53
+// bigint (very bid number and computation used in trading and stock market things)
+// symbol (mainly used while working on react as to find uniqueness used by figma tools(what an individual component is ))
+
+// non-primitive datatype
+
+
+// typeof datatypes:
+
+console.log(typeof(undefined));// undefined
+console.log(typeof(null));// object
+console.log(typeof(bigint));
+console.log(typeof(symbol));
